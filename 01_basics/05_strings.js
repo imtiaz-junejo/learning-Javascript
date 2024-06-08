e50d5897cc9e2ac9dea707676b826c95e702f9b8
Imtiaz Ali <142862184+imtiaz-junejo@users.noreply.github.com>
2024-06-08 05:03:53 +0000

    done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,1 +1,6 @@
-console.log("Imtiaz");
+let name = "Imtiaz"
+let age = 20
+
+// console.log("Hi "+ name + " your age is "+ age); // this is not recommended.
+console.log(`Hi ${name}, your age is ${age}.`);
+
